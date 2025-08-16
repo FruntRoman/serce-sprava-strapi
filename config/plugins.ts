783f3186757c26842cf1f05e18dd851da65466b2
Frunt <lncubus7654@gmail.com>
2025-08-17 02:08:40 +0300

    cloud setup

--- config/plugins.ts
+++ config/plugins.ts
@@ -1,1 +1,16 @@
-export default () => ({});
+export default ({ env }) => ({
+  upload: {
+    config: {
+      provider: '@strapi/provider-upload-cloudinary',
+      providerOptions: {
+        cloud_name: env('du9mlvwvw'),
+        api_key: env('239631828948374'),
+        api_secret: env('fwJReZERLg9YwpwIEl7h98Tip6c'),
+      },
+      actionOptions: {
+        upload: {},
+        delete: {},
+      },
+    },
+  },
+});
